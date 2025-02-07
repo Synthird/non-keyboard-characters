61b9d9995d441e19e35e40ed8ccd73f80d2afba5
Synthird <105394020+Synthird@users.noreply.github.com>
2025-02-06 21:30:23 -0800

    Added a border around the tip

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,16 @@
 const htmlPage = document.getElementsByTagName("html")[0];
 const checkbox = document.getElementById("toggle-theme");
+const tip = document.getElementById("tip");
+
+const darkModeBorderColour = tip.style.borderColor;
+const lightModeBorderColour = "rgb(133, 133, 133)";
 
 checkbox.addEventListener("click", () => {
 	if (checkbox.checked) {
 		document.getElementsByTagName("html")[0].style.colorScheme = "dark";
+		tip.style.borderColor = darkModeBorderColour;
 	} else {
 		document.getElementsByTagName("html")[0].style.colorScheme = "light";
+		tip.style.borderColor = lightModeBorderColour;
 	}
 });
