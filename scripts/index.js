69fda0c34417ca461f9ead01d3a2657deddcb187
Synthird <105394020+Synthird@users.noreply.github.com>
2025-02-07 21:30:18 -0800

    Removed and took advantage of unused variables

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,8 @@
 const htmlPage = document.getElementsByTagName("html")[0];
 const checkbox = document.getElementById("toggle-theme");
-const tip = document.getElementById("tip");
 
 function setTheme(themeName) {
-	document.getElementsByTagName("html")[0].style.colorScheme = themeName;
+	htmlPage.style.colorScheme = themeName;
 }
 
 checkbox.addEventListener("click", () => {
